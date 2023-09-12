a1598d8925f1a907b1ac41572a9c185b67af7688
古川　美桜菜 <furukawa_m@thinkone.jp>
2023-09-12 14:52:55 +0900

    purchaseの設定

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Purchase;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,17 +16,37 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            ItemSeeder::class
+        ]);
+
         \App\Models\Customer::factory(1000)->create();
+        \App\Models\Purchase::factory(100)->create([
+            'customer_id' => function () {
+                return \App\Models\Customer::inRandomOrder()->first()->id;
+            },
+        ]);
+
+
+        // $items = \App\Models\Item::all();
+
+        // Purchase::factory(100)->create()
+        // ->each(function(Purchase $purchase) use ($items) {
+        //     $purchase->items()->attach(
+        //         $items->random(rand(1, 3))->pluck('id')->toArray(),
+        //         [ 'quantity' => rand(1, 5) ]
+        //     );
+        // });
+
+        // \App\Models\User::factory(10)->create();
+        // \App\Models\Purchase::factory(1)->create();
+        // \App\Models\Customer::factory(1000)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
 
-        $this->call([
-            UserSeeder::class,
-            ItemSeeder::class
-        ]);
     }
 }
